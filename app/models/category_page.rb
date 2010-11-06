291844801f948d4c3a57f3000e41c27e4e129545
AJ Siegel <ritprez@gmail.com>
2010-11-06 18:11:17 -0400

    added random photo tag

--- app/models/category_page.rb
+++ app/models/category_page.rb
@@ -1,3 +1,5 @@
+require "net/http"
+require "uri"
 class CategoryPage < Page
 
 	desc %{
@@ -9,13 +11,47 @@ class CategoryPage < Page
     clean_url "#{ url }/"
   end
 
-def find_by_url(url, live = true, clean = false)
+  def find_by_url(url, live = true, clean = false)
     url = clean_url(url) if clean
     if url =~ %r{^#{ self.url }(\d{1,6})/?$}
       children.find_by_class_name('ProductPage')
     else
       super
     end
+  end
+  
+  tag "a_photo" do |tag|
+    output = ''
+    
+    url = "http://api.flickr.com/services/rest/?method=flickr.collections.getTree&collection_id=72157624659199715&api_key=6f977fc52e81b8f45764d91ab84999c2&format=json&nojsoncallback=1&user_id=52768084@N06"
+    
+    json = Net::HTTP.get(URI.parse(url))
+
+    result = ActiveSupport::JSON.decode(json)
+    
+    unless result['stat'] == "fail" 
+      set = result['collections']['collection'][0]['set']
+      index = rand(set.length+1)
+      
+      photoset_id = set[index]['id']
+      
+      set_url = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=#{photoset_id}&api_key=6f977fc52e81b8f45764d91ab84999c2&format=json&nojsoncallback=1"
+      
+      json = Net::HTTP.get(URI.parse(set_url))
+
+      result = ActiveSupport::JSON.decode(json)
+    
+      unless result['stat'] == "fail" 
+        photoset = result['photoset']['photo']
+        photo = photoset[rand(photoset.length+1)]
+        src = "http://farm#{photoset[0]['farm']}.static.flickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_m.jpg"
+       
+        output = "<img src=#{src} alt='Random Photo' />"
+      end
+    end
+    
+    
+    
   end
 end
 
